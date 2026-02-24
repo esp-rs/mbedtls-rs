b001e6e5df5687e215fc21c17e33c3515ec5db2d
Eric Schaal <eric.schaal@my01.io>
2026-02-24 11:22:41 -0500

    chore: update example description

--- examples/esp/src/bin/time_verification.rs
+++ examples/esp/src/bin/time_verification.rs
@@ -1,7 +1,8 @@
-//! Example of a client connection to a server, using the async API.
+//! Example demonstrating certificate time validation using the async API.
 //!
-//! This example connects to `https://httpbin.org/ip` and then to `https://certauth.cryptomix.com/json/` (mTLS)
-//! and performs a simple HTTPS 1.0 GET request to each.
+//! This example connects to `https://httpbin.org/ip` twice to verify that:
+//! 1. Certificate validation succeeds with correct time
+//! 2. Certificate validation fails when time is manipulated (simulating an expired certificate)
 
 #![no_std]
 #![no_main]
