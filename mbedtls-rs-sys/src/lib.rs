ericschaal/esp-mbedtls

wip: provide time support to mbedtls

esp-mbedtls-sys/src/lib.rs
@@ -16,6 +16,8 @@ mod extra_impls; // TODO: Figure out if we still need this
 #[cfg(not(target_os = "espidf"))]
 pub mod accel;
 #[cfg(not(target_os = "espidf"))]
+pub mod time;
+#[cfg(not(target_os = "espidf"))]
 pub mod hook;
 pub mod self_test;
 

esp-rs/mbedtls-rs

mbedtls-rs-sys/src/lib.rs
@@ -16,8 +16,12 @@ mod extra_impls; // TODO: Figure out if we still need this
 #[cfg(not(target_os = "espidf"))]
 pub mod accel;
 #[cfg(not(target_os = "espidf"))]
+pub mod clock;
+#[cfg(not(target_os = "espidf"))]
 pub mod hook;
 pub mod self_test;
+#[cfg(not(target_os = "espidf"))]
+pub mod timer;
 
 #[allow(
     non_camel_case_types,